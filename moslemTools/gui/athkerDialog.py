4a090b707520e3a34d69aac54ba46702589a565b
mesteranas <135442710+mesteranas@users.noreply.github.com>
2024-10-31 12:48:28 +0300

    added names of allah ,update athker and fixed problems

--- moslemTools/gui/athkerDialog.py
+++ moslemTools/gui/athkerDialog.py
@@ -1,4 +1,4 @@
-import time
+import time,winsound
 import PyQt6.QtWidgets as qt
 import PyQt6.QtGui as qt1
 import PyQt6.QtCore as qt2
@@ -20,9 +20,22 @@ class AthkerDialog (qt.QDialog):
         self.athkerList=athkerList
         self.athkerViewer=guiTools.QListWidget()
         for thker in self.athkerList:
-            self.athkerViewer.addItem(thker["text"])
+            self.athkerViewer.addItem(thker["text"] + " " + str(thker["times"]) + _(" مرات"))
         layout.addWidget(self.athkerViewer)
         self.athkerViewer.clicked.connect(self.onPlay)
+        qt1.QShortcut("escape",self).activated.connect(lambda:self.closeEvent(None))
+        qt1.QShortcut("ctrl+c",self).activated.connect(self.copy_selected_item)
+        qt1.QShortcut("ctrl+a",self).activated.connect(self.copy_all_items)
+    def copy_all_items(self):
+        all_text="\n".join([self.athkerViewer.item(i).text() for i in range(self.athkerViewer.count())])
+        guiTools.clikboard.copyText(all_text)
+        winsound.Beep(1000,100)
+    def copy_selected_item(self):
+        selected_item=self.athkerViewer.currentItem()
+        if selected_item:
+            guiTools.clikboard.copyText(selected_item.text())
+            winsound.Beep(1000,100)
+
     def onPlay(self):
         if self.media.isPlaying():
             self.media.stop()
@@ -33,3 +46,6 @@ class AthkerDialog (qt.QDialog):
             else:
                 self.media.setSource(url)
             self.media.play()
+    def closeEvent (self,event):
+        self.media.stop()
+        self.close()
