4e3e7b30bc1a2c30377b6d8e54498e1cb0537477
mesteranas <135442710+mesteranas@users.noreply.github.com>
2024-11-29 11:39:58 +0200

    added startup

--- moslemTools/settings/tabs/genral.py
+++ moslemTools/settings/tabs/genral.py
@@ -1,3 +1,6 @@
+import os
+import sys
+import winreg as reg
 import gui
 import gettext
 _=gettext.gettext
@@ -23,9 +26,67 @@ class Genral(qt.QWidget):
         layout1.addWidget(label)
         layout1.addWidget(self.language)
         layout1.addWidget(self.ExitDialog)
+        self.startup=qt.QCheckBox(_("بدء تشغيل البرنامج عند بدء تشغيل النظام"))
+        self.startup.setChecked(self.check_in_startup())
+        self.startup.checkStateChanged.connect(self.onStartupChanged)
+        layout1.addWidget(self.startup)
         self.reciter=qt.QComboBox()
         self.reciter.addItems(gui.reciters.keys())
         self.reciter.setCurrentIndex(int(settings_handler.get("g","reciter")))
         self.reciter.setAccessibleName(_("تحديد القارئ للقرآن المكتوب"))
         layout1.addWidget(qt.QLabel(_("تحديد القارئ للقرآن المكتوب")))
         layout1.addWidget(self.reciter)
+
+    def add_to_startup(self):
+        try:
+            app_name = app.appName
+            app_path = sys.executable
+            key = reg.OpenKey(reg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Run", 0, reg.KEY_SET_VALUE)
+            reg.SetValueEx(key, app_name, 0, reg.REG_SZ, app_path)
+            reg.CloseKey(key)
+
+            
+        except Exception as e:
+            qt.QMessageBox.critical(self, _("خطأ"),_("تعظر اتمام العملية"))
+
+    def check_in_startup(self):
+        try:
+            app_name = app.appName
+            key = reg.OpenKey(reg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Run", 0, reg.KEY_READ)
+            index = 0
+
+            while True:
+                try:
+                    name, value, _ = reg.EnumValue(key, index)
+                    if name == app_name:
+                        return True
+                        reg.CloseKey(key)
+                        return
+                    index += 1
+                except OSError:
+                    break
+
+            reg.CloseKey(key)
+            return False
+        except Exception as e:
+            return False
+
+    def remove_from_startup(self):
+        try:
+            app_name = app.appName
+            key = reg.OpenKey(reg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Run", 0, reg.KEY_SET_VALUE)
+            reg.DeleteValue(key, app_name)
+            reg.CloseKey(key)
+
+            
+        except FileNotFoundError:
+            qt.QMessageBox.critical(self, _("خطأ"),_("تعظر اتمام العملية"))
+        except Exception as e:
+            qt.QMessageBox.critical(self, _("خطأ"),_("تعظر اتمام العملية"))
+
+
+    def onStartupChanged(self,value):
+        if value:
+            self.add_to_startup()
+        else:
+            self.remove_from_startup()
