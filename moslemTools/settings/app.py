0986cb5d673d5f6cc7001ce9c6b00b50bccaa230
abd alrhman mohamed, alcoder <162161775+MesterAbdAlrhmanMohmed@users.noreply.github.com>
2024-12-12 18:35:24 -0800

    v 3.0

--- moslemTools/settings/app.py
+++ moslemTools/settings/app.py
@@ -1,6 +1,6 @@
 name="moslem tools"
 appName="moslemTools_GUI"
 appdirname="moslemTools"
-version=2.0
+version=3.0
 description="this app contains all things about islam like read and liston to quran kreem , view prayer times and more"
-creater="mesteranas and abd alrahman mohamed alcoder"
+creater="abd alrhman mohamed alcoder, anas mohamed"
