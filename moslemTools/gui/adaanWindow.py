2bc8f3001abe54bc78cf6f422ca98349de38839f
mesteranas <135442710+mesteranas@users.noreply.github.com>
2024-12-14 19:03:47 +0200

    fixed adaan problems

--- moslemTools/gui/adaanWindow.py
+++ moslemTools/gui/adaanWindow.py
@@ -8,10 +8,11 @@ class AdaanDialog(qt.QDialog):
         self.setWindowTitle(title)
         self.setWindowIcon(qt1.QIcon("data/icons/app_icon.jpg"))
         self.media_player=QMediaPlayer()
+        self.media_player.mediaStatusChanged.connect(self.onStateChanged)
         self.audio_output=QAudioOutput()
         self.media_player.setAudioOutput(self.audio_output)
         if index==0:
-            self.media_player.setSource(qt2.QUrl.fromLocalFile("data/sounds/adaan/fajr.webm"))
+            self.media_player.setSource(qt2.QUrl.fromLocalFile("data/sounds/adaan/fajr.mp3"))
         else:
             self.media_player.setSource(qt2.QUrl.fromLocalFile("data/sounds/adaan/genral.webm"))
         self.media_player.play()
@@ -19,3 +20,6 @@ class AdaanDialog(qt.QDialog):
     def closeEvent(self,event):
         self.media_player.stop()
         self.accept()
+    def onStateChanged(self,state):
+        if state==self.media_player.MediaStatus.EndOfMedia :
+            self.accept()
