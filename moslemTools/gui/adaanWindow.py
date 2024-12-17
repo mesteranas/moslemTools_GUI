74c3f3c11b45a54844a8ea747df989c3ba939570
abd alrhman mohamed, alcoder <162161775+MesterAbdAlrhmanMohmed@users.noreply.github.com>
2024-12-17 14:48:11 -0800

    edit in adaan window and added after adaan window

--- moslemTools/gui/adaanWindow.py
+++ moslemTools/gui/adaanWindow.py
@@ -5,9 +5,12 @@ from PyQt6.QtMultimedia import QAudioOutput,QMediaPlayer
 class AdaanDialog(qt.QDialog):
     def __init__(self,p,index:int,title:str):
         super().__init__(p)
-        self.showFullScreen()
+        self.setGeometry(100,100,400,400)
         self.setWindowTitle(title)
-        self.setWindowIcon(qt1.QIcon("data/icons/app_icon.jpg"))
+        self.lay=qt.QLabel()
+        self.lay.setGeometry(50,50,300,300)
+        self.lay.setPixmap(qt1.QPixmap("data/icons/app_icon.jpg"))
+        self.lay.setScaledContents(True)
         self.media_player=QMediaPlayer()
         self.media_player.mediaStatusChanged.connect(self.onStateChanged)
         self.audio_output=QAudioOutput()
@@ -18,9 +21,14 @@ class AdaanDialog(qt.QDialog):
             self.media_player.setSource(qt2.QUrl.fromLocalFile("data/sounds/adaan/genral.webm"))
         self.media_player.play()
         qt1.QShortcut("escape",self).activated.connect(self.close)
+        layout=qt.QVBoxLayout(self)
+        layout.addWidget(self.lay)
     def closeEvent(self,event):
         self.media_player.stop()
         self.accept()
     def onStateChanged(self,state):
-        if state==self.media_player.MediaStatus.EndOfMedia :
-            self.accept()
+        if state==self.media_player.MediaStatus.EndOfMedia:            
+            self.accept()            
+            from afterAdaan import AfterAdaan
+            window=AfterAdaan(self)
+            window.exec()
---
+++ moslemTools/gui/afterAdaan.py
@@ -0,0 +1,16 @@
+from PyQt6 import QtWidgets as qt
+from PyQt6 import QtCore as qt2
+class AfterAdaan(qt.QDialog):
+    def __init__(self):
+        super().__init__()
+        self.resize(1200,600)
+        self.setWindowTitle(_("دعاء بعد الأذان"))        
+        self.suplication=qt.QTextEdit()
+        self.suplication.setReadOnly(True)                                    
+        self.suplication.setTextInteractionFlags(qt2.Qt.TextInteractionFlag.TextSelectableByMouse|qt2.Qt.TextInteractionFlag.TextSelectableByKeyboard)        
+        self.suplication.setLineWrapMode(qt.QTextEdit.LineWrapMode.NoWrap)
+        self.suplication.setAcceptRichText(True)
+        self.suplication.setAccessibleName(_("دعاء بعد الأذان"))
+        self.suplication.setText(_("اللَّهُمَّ رَبَّ هَذِهِ الدَّعْوَةِ التَّامَّةِ، وَالصَّلَاةِ القَائِمَةِ، آتِ مُحَمَّدًا الوَسِيلَةَ وَالفَضِيلَةَ، وَابْعَثْهُ مَقَامًا مَحْمُودًا الَّذِي وَعَدْتَهُ\nيُستحب إضافة: إِنَّكَ لَا تُخْلِفُ المِيعَادَ.\n\nثواب الدعاء بعد الأذان\nقال رسول الله صل الله عليه وسلم: مَنْ قَالَ حِينَ يَسْمَعُ النِّدَاءَ: اللَّهُمَّ رَبَّ هَذِهِ الدَّعْوَةِ التَّامَّةِ، وَالصَّلَاةِ القَائِمَةِ، آتِ مُحَمَّدًا الوَسِيلَةَ وَالفَضِيلَةَ، وَابْعَثْهُ مَقَامًا مَحْمُودًا الَّذِي وَعَدْتَهُ؛ حَلَّتْ لَهُ شَفَاعَتِي يَوْمَ القِيَامَةِ\nرواه البخاري في صحيحه."))
+        layout=qt.QVBoxLayout(self)
+        layout.addWidget(self.suplication)
