33eaa2ecdab9e5c486b70ccf66c7ddbbb7580c31
abd alrhman mohamed, alcoder <162161775+MesterAbdAlrhmanMohmed@users.noreply.github.com>
2024-12-18 17:18:06 -0800

    edit in azan window

--- moslemTools/gui/adaanWindow.py
+++ moslemTools/gui/adaanWindow.py
@@ -2,6 +2,7 @@ import PyQt6.QtWidgets as qt
 import PyQt6.QtGui as qt1
 import PyQt6.QtCore as qt2
 from PyQt6.QtMultimedia import QAudioOutput,QMediaPlayer
+from .after_azaan import AfterAdaan
 class AdaanDialog(qt.QDialog):
     def __init__(self,p,index:int,title:str):
         super().__init__(p)
@@ -9,7 +10,7 @@ class AdaanDialog(qt.QDialog):
         self.setWindowTitle(title)
         self.lay=qt.QLabel(title)
         self.lay.setAlignment(qt2.Qt.AlignmentFlag.AlignCenter)
-        self.lay.setStyleSheet("font-size:150px;")
+        self.lay.setStyleSheet("font-size:100px;")
         self.media_player=QMediaPlayer()
         self.media_player.mediaStatusChanged.connect(self.onStateChanged)
         self.audio_output=QAudioOutput()
@@ -28,6 +29,5 @@ class AdaanDialog(qt.QDialog):
     def onStateChanged(self,state):
         if state==self.media_player.MediaStatus.EndOfMedia:            
             self.accept()            
-            from after_azaan import AfterAdaan
             window=AfterAdaan(self)
             window.exec()
