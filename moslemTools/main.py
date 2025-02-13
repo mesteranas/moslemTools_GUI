bab934e10daa8474e5c4451e177d645f41c4d0b4
abd alrhman mohamed, alcoder <162161775+MesterAbdAlrhmanMohmed@users.noreply.github.com>
2025-02-13 01:42:45 -0800

    edit the appearance of the program

--- moslemTools/main.py
+++ moslemTools/main.py
@@ -1,93 +1,105 @@
 import sys
 from custome_errors import *
-sys.excepthook=my_excepthook
-import update,guiTools,json,random,os
+sys.excepthook = my_excepthook
+import update, guiTools, json, random, os
 from settings import *
 import PyQt6.QtWidgets as qt
 import PyQt6.QtGui as qt1
 import PyQt6.QtCore as qt2
-from PyQt6.QtMultimedia import QAudioOutput,QMediaPlayer
+from PyQt6.QtMultimedia import QAudioOutput, QMediaPlayer
 from appTabs import *
 language.init_translation()
 class main(qt.QMainWindow):
     def __init__(self):
-        super().__init__()        
-        self.setWindowTitle(app.name+_("الإصدار:") + str(app.version))
+        super().__init__()
+        self.setWindowTitle(app.name + _("الإصدار:") + str(app.version))
         self.resize(1200,600)
         self.media_player=QMediaPlayer()
         self.audio_output=QAudioOutput()
         self.media_player.setAudioOutput(self.audio_output)
         self.timer=qt2.QTimer(self)
         self.timer.timeout.connect(self.random_audio_theker)        
-        layout=qt.QVBoxLayout()        
-        self.tools=qt.QTabWidget()
-        self.tools.addTab(prayer_times(),_("مواقيت الصلاة والتاريخ"))
-        self.tools.addTab(Quran(),_("القرآن الكريم مكتوب"))        
-        self.tools.addTab(QuranPlayer(),_("القرآن الكريم صوتي"))        
-        self.tools.addTab(hadeeth(),_("الأحاديث النبوية والقدسية"))        
-        self.tools.addTab(book_marcks(),_("العلامات المرجعية"))                        
-        self.tools.addTab(Albaheth(),_("الباحث في القرآن والأحاديث"))
-        self.tools.addTab(protcasts(),(_("الإذاعات الإسلامية")))        
-        self.tools.addTab(Athker(),_("الأذكار والأدعية"))                                        
-        self.tools.addTab(sibha(),(_("سبحة إلكترونية")))
-        self.tools.addTab(NamesOfAllah(),_("أسماء الله الحُسْنى"))
-        self.tools.addTab(DateConverter(),(_("محول التاريخ")))        
-        self.tools.addTab(UserGuide(),(_("دليل المستخدم")))
-        self.tools.addTab(About_developers(),(_("عن المطورين")))                
+        layout=qt.QVBoxLayout()                
+        content_layout=qt.QHBoxLayout()
+        self.list_widget=qt.QListWidget()
+        self.stacked_widget=qt.QStackedWidget()        
+        tabs=[
+            (prayer_times, _("مواقيت الصلاة والتاريخ")),
+            (Quran, _("القرآن الكريم مكتوب")),
+            (QuranPlayer, _("القرآن الكريم صوتي")),
+            (hadeeth, _("الأحاديث النبوية والقدسية")),
+            (book_marcks, _("العلامات المرجعية")),
+            (Albaheth, _("الباحث في القرآن والأحاديث")),
+            (protcasts, _("الإذاعات الإسلامية")),
+            (Athker, _("الأذكار والأدعية")),
+            (sibha, _("سبحة إلكترونية")),
+            (NamesOfAllah, _("أسماء الله الحُسْنى")),
+            (DateConverter, _("محول التاريخ")),
+            (UserGuide, _("دليل المستخدم")),
+            (About_developers, _("عن المطورين"))
+        ]
+        for widget_class, label in tabs:
+            self.list_widget.addItem(label)
+            instance=widget_class()
+            self.stacked_widget.addWidget(instance)        
+        self.list_widget.currentRowChanged.connect(self.stacked_widget.setCurrentIndex)
+        self.list_widget.setCurrentRow(0)        
+        content_layout.addWidget(self.list_widget, 1)
+        content_layout.addWidget(self.stacked_widget, 3)
+        layout.addLayout(content_layout)        
+        self.setting=guiTools.QPushButton(_("الإعدادات"))
+        self.setting.clicked.connect(lambda: settings(self).exec())
+        layout.addWidget(self.setting)        
+        w=qt.QWidget()
+        w.setLayout(layout)
+        self.setCentralWidget(w)                
         self.tray_icon=qt.QSystemTrayIcon(self)
         self.tray_icon.setIcon(qt1.QIcon("data/icons/app_icon.jpg"))
         self.tray_icon.setToolTip(app.name)
         self.tray_menu=qt.QMenu(self)
         self.random_thecker_audio=qt1.QAction(_("تشغيل ذكر عشوائي"))
-        self.random_thecker_audio.triggered.connect(self.random_audio_theker)        
+        self.random_thecker_audio.triggered.connect(self.random_audio_theker)
         self.random_thecker_text=qt1.QAction(_("عرض ذكر عشوائي"))
-        self.random_thecker_text.triggered.connect(self.show_random_theker)                
-        self.show_action = qt1.QAction(_("إخفاء البرنامج"))
-        self.show_action.triggered.connect(self.toggle_visibility)        
+        self.random_thecker_text.triggered.connect(self.show_random_theker)
+        self.show_action=qt1.QAction(_("إخفاء البرنامج"))
+        self.show_action.triggered.connect(self.toggle_visibility)
         self.close_action=qt1.QAction(_("إغلاق البرنامج"))
-        self.close_action.triggered.connect(lambda:qt.QApplication.quit())
+        self.close_action.triggered.connect(lambda: qt.QApplication.quit())
         self.tray_menu.addAction(self.random_thecker_audio)
         self.tray_menu.addAction(self.random_thecker_text)
-        self.tray_menu.addAction(self.show_action)        
+        self.tray_menu.addAction(self.show_action)
         self.tray_menu.addAction(self.close_action)
         self.tray_icon.setContextMenu(self.tray_menu)
-        self.tray_icon.show()    
+        self.tray_icon.show()
         self.TIMER1=qt2.QTimer(self)
         self.TIMER1.timeout.connect(self.show_random_theker)        
-        layout.addWidget(self.tools)            
-        self.setting=guiTools.QPushButton(_("الإعدادات"))
-        self.setting.clicked.connect(lambda: settings(self).exec())        
-        layout.addWidget(self.setting)
-        w=qt.QWidget()
-        w.setLayout(layout)
-        self.setCentralWidget(w)
         self.runAudioThkarTimer()
-        self.notification_random_thecker()        
-        if settings_handler.get("update","autoCheck")=="True":
-            update.check(self,message=False)                    
-    def toggle_visibility(self):        
+        self.notification_random_thecker()
+        if settings_handler.get("update", "autoCheck") == "True":
+            update.check(self, message=False)            
+    def toggle_visibility(self):
         if self.isVisible():
             self.hide()
             self.show_action.setText(_("إظهار البرنامج"))
         else:
             self.show()
-            self.show_action.setText(_("إخفاء البرنامج"))
+            self.show_action.setText(_("إخفاء البرنامج"))    
     def show_random_theker(self):
-        with open("data/json/text_athkar.json","r",encoding="utf_8") as f:
+        with open("data/json/text_athkar.json", "r", encoding="utf_8") as f:
             data=json.load(f)
         random_theckr=random.choice(data)
-        guiTools.SendNotification(_("ذكر عشوائي"),random_theckr)
+        guiTools.SendNotification(_("ذكر عشوائي"), random_theckr)    
     def notification_random_thecker(self):
         self.TIMER1.stop()
-        if formatDuration("athkar","text")!=0:
-            self.TIMER1.start(formatDuration("athkar","text"))
+        if formatDuration("athkar", "text") != 0:
+            self.TIMER1.start(formatDuration("athkar", "text"))    
     def runAudioThkarTimer(self):
         self.timer.stop()
-        if formatDuration("athkar","voice")!=0:
-            self.timer.start(formatDuration("athkar","voice"))    
+        if formatDuration("athkar", "voice") != 0:
+            self.timer.start(formatDuration("athkar", "voice"))    
     def closeEvent(self, event):
         if app.exit:
-            if settings_handler.get("g","exitDialog")=="True":
+            if settings_handler.get("g", "exitDialog") == "True":
                 m=guiTools.ExitApp(self)
                 m.exec()
                 if m:
@@ -95,15 +107,15 @@ class main(qt.QMainWindow):
             else:
                 self.close()
         else:
-            self.close()
-    def random_audio_theker(self):        
+            self.close()    
+    def random_audio_theker(self):
         folder_path=r"data\sounds\athkar"
         sound_files=[f for f in os.listdir(folder_path) if f.endswith(('.ogg'))]
         if sound_files:
             chosen_file=random.choice(sound_files)
-            file_path=os.path.join(folder_path,chosen_file)
+            file_path=os.path.join(folder_path, chosen_file)
             self.media_player.setSource(qt2.QUrl.fromLocalFile(file_path))
-            self.media_player.play()    
+            self.media_player.play()
 App=qt.QApplication([])
 App.setApplicationDisplayName(app.name)
 App.setApplicationName(app.name)
@@ -113,4 +125,4 @@ App.setWindowIcon(qt1.QIcon("data/icons/app_icon.jpg"))
 App.setStyle('fusion')
 window=main()
 window.show()
-App.exec()    
+App.exec()
