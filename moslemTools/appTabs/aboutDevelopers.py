47c75d8ed7df5b345141340b8adbb21858c6ec4e
abd alrhman mohamed, alcoder <162161775+MesterAbdAlrhmanMohmed@users.noreply.github.com>
2025-03-27 21:54:56 +0200

    Video explaning the program was deleted

--- moslemTools/appTabs/aboutDevelopers.py
+++ moslemTools/appTabs/aboutDevelopers.py
@@ -18,8 +18,7 @@ class About_developers(qt.QDialog):
         self.info.addItem(_("قناة أنس على telegram"))
         self.info.addItem(_("حساب أنس على telegram"))
         self.info.addItem(_("حساب أنس على GitHub"))
-        self.info.addItem(_("التواصل مع أنس عبر البريد الإكتروني"))        
-        self.info.addItem(_("فيديو شرح البرنامج على YouTube"))
+        self.info.addItem(_("التواصل مع أنس عبر البريد الإكتروني"))                
         self.info_text=qt.QLineEdit()
         self.info_text.setReadOnly(True)
         self.info_text.setText(_("اللهم اجعل عملنا هذا في ميزان حسناتنا وصدقة جارية لنا"))
@@ -45,6 +44,4 @@ class About_developers(qt.QDialog):
             elif text == _("حساب أنس على GitHub"):
                 webbrowser.open("https://github.com/mesteranas/")
             elif text == _("التواصل مع أنس عبر البريد الإكتروني"):
-                webbrowser.open("mailto:anasformohammed@gmail.com")
-            elif text == _("فيديو شرح البرنامج على YouTube"):
-                webbrowser.open("https://www.youtube.com/watch?v=NUe8ftmg4Cw")
+                webbrowser.open("mailto:anasformohammed@gmail.com")            
