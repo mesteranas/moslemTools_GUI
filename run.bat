65ea8c66fcb4844d68eb2f35f5f043109a43eaa4
mesteranas <135442710+mesteranas@users.noreply.github.com>
2025-03-27 22:17:06 +0200

    added run.bat file

---
+++ run.bat
@@ -0,0 +1,15 @@
+  @echo off
+set VENV_NAME=mtvenv
+set REQUIREMENTS=requirements.txt
+set PYTHON_EXEC=python
+set SCRIPT_NAME=moslem_tools.py
+if not exist %VENV_NAME% (
+    echo Virtual environment not found. Creating...
+    %PYTHON_EXEC% -m venv %VENV_NAME%
+    echo Installing requirements...
+    call %VENV_NAME%\Scripts\activate && pip install -r %REQUIREMENTS%
+)
+echo runing moslem tools ...
+call %VENV_NAME%\Scripts\activate
+cd moslemTools
+%PYTHON_EXEC% %SCRIPT_NAME%
