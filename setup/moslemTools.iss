8c01b0cc0cc56d4fdfb5045b38f1e84851de5847
mesteranas <135442710+mesteranas@users.noreply.github.com>
2025-03-27 15:20:42 +0200

    update some setup files

---
+++ setup/moslemTools.iss
@@ -0,0 +1,98 @@
+﻿#define MyAppName "moslemTools"
+#define MyAppVersion "7.0.0"
+#define MyAppPublisher "mister anas , abd el-rahman mohammed alcoder"
+#define MyAppURL "https://github.com/mesteranas/moslemTools_GUI/"
+#define MyAppExeName "moslem_tools.exe"
+
+[Setup]
+AppName={#MyAppName}
+AppId={{B1A04228-9227-4658-8536-AF1D4C6D1F55}}
+AppVersion={#MyAppVersion }
+;AppVersion={#MyAppVersion}
+VersionInfoDescription=moslemTools , the best program for moslems
+AppPublisher={#MyAppPublisher }
+VersionInfoVersion={#MyAppVersion}
+VersionInfoCompany={#MyAppPublisher }
+VersionInfoCopyright=copyright, ©2025 moslem tools developers
+VersionInfoProductName=moslemTools
+VersionInfoProductVersion={#MyAppVersion}
+VersionInfoOriginalFileName=moslemTools_Setup.exe
+AppPublisherURL={#MyAppURL}
+AppSupportURL={#MyAppURL}
+AppUpdatesURL={#MyAppURL}
+ArchitecturesAllowed=x64
+SetupIconFile=data\icons\app_icon.ico
+DefaultDirName={sd}\program files\{#MyAppName}
+DisableProgramGroupPage=yes
+PrivilegesRequired=admin
+OutputDir=moslemTools_build
+OutputBaseFilename=moslemToolsSetup
+LicenseFile=data\help\LICENSE.txt
+Compression=lzma
+CloseApplications=force
+restartApplications=yes
+SolidCompression=yes
+WizardStyle=modern
+DisableWelcomePage=no
+
+ArchitecturesInstallIn64BitMode=x64
+MinVersion=0,6.2
+[Languages]
+Name: "english"; MessagesFile: "compiler:Default.isl"
+[CustomMessages]
+english.DeleteDataPrompt=Do you want to delete the settings and the downloaded data?
+
+[Tasks]
+Name: "desktopicon"; Description: "{cm:CreateDesktopIcon}"; GroupDescription: "{cm:AdditionalIcons}"
+[Files]
+Source: "moslemTools_build\{#MyAppExeName}"; DestDir: "{app}"; Flags: ignoreversion
+Source: "moslemTools_build\*"; DestDir: "{app}"; Flags: ignoreversion recursesubdirs createallsubdirs
+
+[Icons]
+Name: "{autoprograms}\moslemTools"; Filename: "{app}\{#MyAppExeName}"; IconFilename: "{app}\data\icons\app_icon.ico"
+Name: "{autodesktop}\moslemTools"; Filename: "{app}\{#MyAppExeName}"; IconFilename: "{app}\data\icons\app_icon.ico"; Tasks: desktopicon
+
+[UninstallRun]
+Filename: "taskkill"; Parameters: "/F /IM moslem_tools.exe"; Flags: runhidden
+
+[UninstallDelete]
+Type: filesandordirs; Name: "{pf}\moslemTools"
+
+[InstallDelete]
+Type: filesandordirs; Name: "{app}\*"
+
+[Run]
+Filename: "{app}\{#MyAppExeName}"; Description: "{cm:LaunchProgram,{#StringChange(MyAppName, '&', '&&')}}"; Flags: nowait postinstall
+
+[Code]
+function InitializeSetup(): Boolean;
+begin
+  Result := True;
+end;
+
+
+procedure CurStepChanged(CurStep: TSetupStep);
+begin
+end;
+
+procedure DeleteSettingsFolder();
+begin
+  DelTree(ExpandConstant('{userappdata}\moslemTools_GUI'), True, True, True);
+end;
+
+function AskDeleteSettingsFolder(): Boolean;
+begin
+  Result := MsgBox(ExpandConstant('{cm:DeleteDataPrompt}'), mbConfirmation, MB_YESNO) = IDYES;
+end;
+
+procedure CurUninstallStepChanged(CurUninstallStep: TUninstallStep);
+begin
+  if CurUninstallStep = usUninstall then
+  begin
+    if AskDeleteSettingsFolder() then
+    begin
+      DeleteSettingsFolder();
+    end;
+  end;
+end;
+
--- setup/setup.py
+++ setup/setup.py
@@ -1,83 +1,149 @@
 import shutil
-import sys
 import os
-import PyQt6.QtPrintSupport
-from cx_Freeze import setup, Executable
-import PyQt6
-pyqt_path = os.path.dirname(PyQt6.__file__)
+import subprocess
+if os.path.exists("moslemTools_build"):
+    print("removing moslemTools_build")
+    shutil.rmtree("moslemTools_build")
+# Create a version resource file for Windows executables.
+version_info = r'''
+# UTF-8
+#
+# For more details about fixed file info 'ffi' see:
+# https://docs.microsoft.com/en-us/windows/win32/menurc/vs-versioninfo
+VSVersionInfo(
+  ffi=FixedFileInfo(
+    filevers=(7, 0, 0, 0),
+    prodvers=(7, 0, 0, 0),
+    mask=0x3f,
+    flags=0x0,
+    OS=0x4,
+    fileType=0x1,
+    subtype=0x0,
+    date=(0, 0)
+  ),
+  kids=[
+    StringFileInfo(
+      [
+        StringTable(
+          '040904B0',
+          [
+            StringStruct('CompanyName', 'mister anas , abd el-rahman mohammed alcoder'),
+            StringStruct('FileDescription', 'moslem tools , the best program for moslems'),
+            StringStruct('FileVersion', '7.0.0.0'),
+            StringStruct('InternalName', 'moslem tools'),
+            StringStruct('OriginalFilename', 'moslem_tools.exe'),
+            StringStruct('ProductName', 'moslem tools'),
+            StringStruct('ProductVersion', '7.0.0.0'),
+            StringStruct('LegalCopyright', '© 2025 ; moslem tools developers')
+          ]
+        )
+      ]
+    ),
+    VarFileInfo([VarStruct('Translation', [1033, 1200])])
+  ]
+)
+'''
+
+# Write version info to a file.
+version_file_path = "version_file.txt"
+with open(version_file_path, "w", encoding="utf-8") as vf:
+    vf.write(version_info.strip())
+
 include_files = [
     ("data/dlls", "data/dlls"),
-    ("data/icons","data/icons"),
-    ("data/sounds/adaan","data/sounds/adaan"),
-    ("data/sounds/athkar","data/sounds/athkar"),
-    ("data/sounds/001001.mp3","data/sounds/001001.mp3"),
-    ("data/sounds/next_page.wav","data/sounds/next_page.wav"),
-    ("data/sounds/prayAfterAdaan.m4a","data/sounds/prayAfterAdaan.m4a"),
-    ("data/sounds/previous_page.wav","data/sounds/previous_page.wav"),
-    ("data/json/files","data/json/files"),
-    ("data/json/quranRecitations","data/json/quranRecitations"),
-    ("data/json/ahadeeth/nawawi40.json","data/json/ahadeeth/nawawi40.json"),
-    ("data/json/ahadeeth/qudsi40.json","data/json/ahadeeth/qudsi40.json"),
-    ("data/json/islamicBooks/elShabahLibe.json","data/json/islamicBooks/elShabahLibe.json"),
-    ("data/json/Quran Translations/en.itani.json","data/json/Quran Translations/en.itani.json"),
-    ("data/json/tafaseer/muyassar.json","data/json/tafaseer/muyassar.json"),
-    ("data/json/athkar.json","data/json/athkar.json"),
-    ("data/json/i raab.json","data/json/i raab.json"),
-    ("data/json/namesOfAllah.json","data/json/namesOfAllah.json"),
-    ("data/json/prophetStories.json","data/json/prophetStories.json"),
-    ("data/json/quran.json","data/json/quran.json"),
-    ("data/json/quranStories.json","data/json/quranStories.json"),
-    ("data/json/reciters.json","data/json/reciters.json"),
-    ("data/json/tanzil.json","data/json/tanzil.json"),
-    ("data/json/text_athkar.json","data/json/text_athkar.json")
+    ("data/icons", "data/icons"),
+    ("data/sounds/adaan", "data/sounds/adaan"),
+    ("data/sounds/athkar", "data/sounds/athkar"),
+    ("data/sounds/001001.mp3", "data/sounds/001001.mp3"),
+    ("data/sounds/next_page.wav", "data/sounds/next_page.wav"),
+    ("data/sounds/prayAfterAdaan.m4a", "data/sounds/prayAfterAdaan.m4a"),
+    ("data/sounds/previous_page.wav", "data/sounds/previous_page.wav"),
+    ("data/json/files", "data/json/files"),
+    ("data/json/quranRecitations", "data/json/quranRecitations"),
+    ("data/json/ahadeeth/nawawi40.json", "data/json/ahadeeth/nawawi40.json"),
+    ("data/json/ahadeeth/qudsi40.json", "data/json/ahadeeth/qudsi40.json"),
+    ("data/json/islamicBooks/elShabahLibe.json", "data/json/islamicBooks/elShabahLibe.json"),
+    ("data/json/Quran Translations/en.itani.json", "data/json/Quran Translations/en.itani.json"),
+    ("data/json/tafaseer/muyassar.json", "data/json/tafaseer/muyassar.json"),
+    ("data/json/athkar.json", "data/json/athkar.json"),
+    ("data/json/i raab.json", "data/json/i raab.json"),
+    ("data/json/namesOfAllah.json", "data/json/namesOfAllah.json"),
+    ("data/json/prophetStories.json", "data/json/prophetStories.json"),
+    ("data/json/quran.json", "data/json/quran.json"),
+    ("data/json/quranStories.json", "data/json/quranStories.json"),
+    ("data/json/reciters.json", "data/json/reciters.json"),
+    ("data/json/tanzil.json", "data/json/tanzil.json"),
+    ("data/json/text_athkar.json", "data/json/text_athkar.json")
 ]
+
+# Dynamically add language resource files from the "data/languages" folder.
 for languageFolder in os.listdir("data/languages"):
-    languagesFolder="data/languages/" + languageFolder
+    languagesFolder = os.path.join("data", "languages", languageFolder)
     if os.path.isdir(languagesFolder):
-        langNameFile=languagesFolder + "/langName.translation"
-        langcontent=languagesFolder + "/LC_MESSAGES/moslemTools_GUI.mo"
-        include_files.append((langNameFile,langNameFile))
-        include_files.append((langcontent,langcontent))
-dll_files = ["Qt6Core.dll", "Qt6Gui.dll", "Qt6Widgets.dll", "Qt6Network.dll","","Qt6Multimedia.dll","Qt6MultimediaQuick.dll","Qt6PrintSupport.dll"]
-for file in dll_files:
-    include_files.append((os.path.join(pyqt_path, "Qt6", "bin", file), os.path.join("lib", file)))
+        langNameFile = os.path.join(languagesFolder, "langName.translation")
+        langContent = os.path.join(languagesFolder, "LC_MESSAGES", "moslemTools_GUI.mo")
+        include_files.append((langNameFile, langNameFile))
+        include_files.append((langContent, langContent))
 
+print("Converting to exe, please wait...")
 
-build_exe_options = {
-    "build_exe": "moslemTools_build",
-    "optimize": 1,
-    "include_files": include_files,
-    "packages": ["appTabs", "functions", "gui","settings","update","guiTools"],
-    "includes": ["PyQt6.QtCore", "PyQt6.QtWidgets", "PyQt6.QtGui", "PyQt6.QtMultimedia", "PyQt6.QtPrintSupport","packaging", "requests","hijri_converter","notifypy","geocoder","pyperclip","win32com.client"],
-    "excludes": ["tkinter", "test", "setuptools", "pip", "numpy", "unittest"],
-    "include_msvcr": True
-}
+# Run PyInstaller with windowed mode, icon, and version file options.
+command = [
+    "pyinstaller",
+    "-w",
+    "--icon=data/icons/app_icon.ico",
+    f"--version-file={version_file_path}",
+    "moslem_tools.py"
+]
 
-setup(
-    name="moslemTools",
-    version="7.0.0",
-    description="moslem tools",
-    long_description="moslem tools , all islamic tools in one place",
-    author="mister anas , abd el-rahman mohamed alcoder",
-    url="https://github.com/mesteranas/moslemTools_GUI/",
-    download_url="https://github.com/mesteranas/moslemTools_GUI/",
-    keywords=["islamic", "islam", "quran", "desktop", "alquran", "hadeeth", "القرآن", "إسلام","حديث"],
-    options={"build_exe": build_exe_options},
-    executables=[
-        Executable(
-            "moslem_tools.py",
-            base="Win32GUI" if sys.platform == "win32" else None,
-            target_name="moslemTools.exe",
-            icon="data/icons/app_icon.ico",
-            copyright="2025 moslem tooles developers"
-        )
-    ]
-)
+run = subprocess.run(command)
 
-folder_paths = ["moslemTools_build/lib/PyQt6/Qt6/bin", "moslemTools_build/lib/PyQt6/Qt6/translations"]
-for folder in folder_paths:
-    try:
-        shutil.rmtree(os.path.abspath(folder))
-    except Exception as e:
-        print(f"Error removing {folder}: {e}")
-
+if run.returncode == 0:
+    print("PyInstaller build successful.")
+    # Assuming that the built executable is in the folder "dist/moslem_tools"
+    target_dir = os.path.join("dist", "moslem_tools")
+    for src, dest in include_files:
+        dest_path = os.path.join(target_dir, dest)
+        dest_folder = os.path.dirname(dest_path)
+        if not os.path.exists(dest_folder):
+            os.makedirs(dest_folder)
+        try:
+            if os.path.isdir(src):
+                if os.path.exists(dest_path):
+                    shutil.rmtree(dest_path)
+                shutil.copytree(src, dest_path)
+            else:
+                shutil.copy2(src, dest_path)
+            print(f"Copied {src} to {dest_path}")
+        except Exception as e:
+            print(f"Error copying {src} to {dest_path}: {e}")
+    print("All include files have been copied.")
+else:
+    print("PyInstaller build failed.")
+print("removing version file")
+try:
+    os.remove(version_file_path )
+    print
+    ("removed")
+except:
+    print("error while removing")
+print("removing build folder")
+try:
+    shutil.rmtree("build")
+    print("done")
+except:
+    print("error")
+print("removing moslem_tools.spec")
+try:
+    os.remove("moslem_tools.spec")
+    print("done")
+except:
+    print("error")
+print("editing some files")
+try:
+    shutil.copytree("dist/moslem_tools","moslemTools_build")
+    print("done")
+    shutil.rmtree("dist")
+    print("done")
+except:
+    print("error")
