1d7fbddbe5fbe5bcc663dddf2b8d1a49dad6e962
abd alrhman mohamed, alcoder <162161775+MesterAbdAlrhmanMohmed@users.noreply.github.com>
2025-04-17 13:21:07 +0200

    edit in main window

--- moslemTools/guiTools/send_notification.py
+++
@@ -1,8 +0,0 @@
-from notifypy import Notify
-from settings import  app
-def SendNotification(title,message):
-	notification = Notify()
-	notification.title = title
-	notification.message = message
-	notification._notification_application_name=app.name
-	notification.send()
