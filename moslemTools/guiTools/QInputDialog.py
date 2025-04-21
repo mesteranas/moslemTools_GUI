be1c9e7f1d9a493578635101247eee459e933d73
abd alrhman mohamed, alcoder <162161775+MesterAbdAlrhmanMohmed@users.noreply.github.com>
2025-04-21 14:51:51 +0200

    edit in QInputDialog layout

--- moslemTools/guiTools/QInputDialog.py
+++ moslemTools/guiTools/QInputDialog.py
@@ -2,9 +2,9 @@ import PyQt6.QtWidgets as qt
 import PyQt6.QtGui as qt1
 from PyQt6.QtCore import Qt
 class QInputDialog(qt.QDialog):
-    def __init__(self, parent, title: str , label: str , text: str = ""):
+    def __init__(self, parent, title: str, label: str, text: str = ""):
         super().__init__(parent)
-        self.resize(500, 150)
+        self.resize(300, 150)
         self.setWindowTitle(title)
         layout = qt.QVBoxLayout(self)
         self.label = qt.QLabel(label)
@@ -17,12 +17,20 @@ class QInputDialog(qt.QDialog):
         self.text.textChanged.connect(self.onTextChanged)
         layout.addWidget(self.text)
         buttonsLayout = qt.QHBoxLayout()
-        self.OKBTN = qt.QPushButton("موافق")
+        self.OKBTN = qt.QPushButton("موافق")        
+        self.OKBTN.setStyleSheet("""
+            background-color: black;
+            color: white;
+        """)
         self.OKBTN.setDisabled(True)
         self.OKBTN.setDefault(True)
         self.OKBTN.clicked.connect(self.accept)
         buttonsLayout.addWidget(self.OKBTN)
         self.cancelBTN = qt.QPushButton("إلغاء")
+        self.cancelBTN.setStyleSheet("""
+            background-color: #8B0000;
+            color: white;
+        """)
         self.cancelBTN.clicked.connect(self.reject)
         buttonsLayout.addWidget(self.cancelBTN)
         qt1.QShortcut("Escape", self).activated.connect(self.reject)
@@ -30,10 +38,21 @@ class QInputDialog(qt.QDialog):
     def closeEvent(self, event):
         self.reject()
         event.accept()
-    def onTextChanged(self, text):
-        self.OKBTN.setDisabled(text.strip() == "")
+    def onTextChanged(self, text):        
+        if text.strip():
+            self.OKBTN.setDisabled(False)
+            self.OKBTN.setStyleSheet("""
+                background-color: green;
+                color: white;
+            """)
+        else:
+            self.OKBTN.setDisabled(True)
+            self.OKBTN.setStyleSheet("""
+                background-color: black;
+                color: white;
+            """)
     @staticmethod
-    def getText(parent, title: str , label: str , text: str = ""):
+    def getText(parent, title: str, label: str, text: str = ""):
         dlg = QInputDialog(parent, title, label, text=text)
         result = dlg.exec()
         if result == qt.QDialog.DialogCode.Accepted:
