49cd4b9e0dd37d66371c54208235ed7b5667bd19
abd alrhman mohamed, alcoder <162161775+MesterAbdAlrhmanMohmed@users.noreply.github.com>
2025-06-03 22:14:46 +0300

    some edits in some texts

--- moslemTools/settings/tabs/locationSettings.py
+++ moslemTools/settings/tabs/locationSettings.py
@@ -57,7 +57,7 @@ class LocationSettings(qt.QWidget):
         self.dl_app1.setStyleSheet("background-color: #0000AA; color: #e0e0e0;")
         self.dl_app1.setVisible(p.cbts(settings_handler.get("location","autoDetect"))==False)
         layout.addWidget(self.dl_app1)
-        self.info=qt.QLabel(_("تنبيه هام، عند تحديد الموقع الجغرافي يجب إعادة تحميل مواقيط الصلاة"))
+        self.info=qt.QLabel(_("تنبيه هام، عند تحديد الموقع الجغرافي يجب إعادة تحميل مواقيت الصلاة"))
         self.info.setAlignment(qt2.Qt.AlignmentFlag.AlignCenter)
         self.info.setFocusPolicy(qt2.Qt.FocusPolicy.StrongFocus)
         layout.addWidget(self.info)
